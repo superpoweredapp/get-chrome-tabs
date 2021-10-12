6357b296a10547cf31fc21aba0a689934501b008
Nikhil Gupta <niks.gupta09@gmail.com>
2021-10-12 12:58:14 -0400

    Try as script

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
-if (process.platform === 'darwin') {
-	const {inspect, promisify} = require('util');
-	const {execFile} = require('child_process');
+if (process.platform === "darwin") {
+	const { inspect, promisify } = require("util");
+	const { execFile } = require("child_process");
 
-	const inspectWithKind = require('inspect-with-kind');
-	const isPlainObj = require('is-plain-obj');
+	const inspectWithKind = require("inspect-with-kind");
+	const isPlainObj = require("is-plain-obj");
 
-	const APP_ERROR = 'Expected `app` option to be either \'canary\' or \'chromium\'';
-	const execFileOption = {timeout: 10000};
+	const APP_ERROR = "Expected `app` option to be either 'canary' or 'chromium'";
+	const execFileOption = { timeout: 10000 };
 	const nameIdMap = new Map([
-		['canary', 'com.google.Chrome.canary'],
-		['chromium', 'org.chromium.Chromium']
+		["canary", "com.google.Chrome.canary"],
+		["chromium", "org.chromium.Chromium"],
 	]);
 	const promisifiedExecFile = promisify(execFile);
 
@@ -21,35 +21,48 @@ if (process.platform === 'darwin') {
 
 		if (argLen === 1) {
 			if (!isPlainObj(option)) {
-				throw new TypeError(`Expected an <Object> to specify get-chrome-tabs option, but got ${
-					inspectWithKind(option)
-				}.`);
+				throw new TypeError(
+					`Expected an <Object> to specify get-chrome-tabs option, but got ${inspectWithKind(
+						option
+					)}.`
+				);
 			}
 
 			if (option.app !== undefined) {
-				if (typeof option.app !== 'string') {
-					throw new TypeError(`${APP_ERROR}, but got a non-string value ${inspectWithKind(option.app)}.`);
+				if (typeof option.app !== "string") {
+					throw new TypeError(
+						`${APP_ERROR}, but got a non-string value ${inspectWithKind(
+							option.app
+						)}.`
+					);
 				}
 
 				if (!nameIdMap.has(option.app)) {
-					throw new RangeError(`${APP_ERROR}, but got neither of them ${inspect(option.app)}.`);
+					throw new RangeError(
+						`${APP_ERROR}, but got neither of them ${inspect(option.app)}.`
+					);
 				}
 			}
 		} else if (argLen !== 0) {
-			throw new RangeError(`Expected 0 or 1 argument ([<Object>]), but got ${argLen} arguments.`);
+			throw new RangeError(
+				`Expected 0 or 1 argument ([<Object>]), but got ${argLen} arguments.`
+			);
 		}
 
-		const id = nameIdMap.get(option.app) || 'com.google.Chrome';
-		const result = JSON.parse((await promisifiedExecFile('osascript', [
-			'-l',
-			'JavaScript',
-			require.resolve('./jxa.js'),
-			id
-		], execFileOption)).stdout);
+		const id = nameIdMap.get(option.app) || "com.google.Chrome";
+		const result = JSON.parse(
+			(
+				await promisifiedExecFile(
+					require.resolve("./jxa.sh"),
+					[id],
+					execFileOption
+				)
+			).stdout
+		);
 
 		if (result.appNotRunning) {
 			const error = new Error(result.message);
-			error.code = 'ERR_APP_NOT_RUNNING';
+			error.code = "ERR_APP_NOT_RUNNING";
 			error.bundleId = id;
 
 			throw error;
@@ -58,13 +71,13 @@ if (process.platform === 'darwin') {
 		return result;
 	};
 } else {
-	const platformName = require('platform-name');
+	const platformName = require("platform-name");
 
 	module.exports = async function getChromeTabs() {
-		const error = new Error(`get-chrome-tabs only supports macOS, but the current platform is ${
-			platformName()
-		}.`);
-		error.code = 'ERR_UNSUPPORTED_PLATFORM';
+		const error = new Error(
+			`get-chrome-tabs only supports macOS, but the current platform is ${platformName()}.`
+		);
+		error.code = "ERR_UNSUPPORTED_PLATFORM";
 
 		throw error;
 	};
